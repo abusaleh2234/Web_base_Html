ac7f16c5b694d76e4302169a2c1aeafbca5d37ac
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-01-31 22:48:31 +0600

    js dom

---
+++ UI-Component/js/index.js
@@ -0,0 +1,5 @@
+// document.getElementById("subTitle").innerText = " sub title"
+// document.getElementById("subTitle").style.border = "1px solid red"
+document.getElementById("subTitle").style.color = "red"
+
+//  document.getElementsByClassName("card_content").style.color ="red"j
