88ce00ef28b0c4d188878ef66eb286d31310e77c
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-01 22:05:30 +0600

    arrow function

---
+++ UI-Component/js/arrow-function.js
@@ -0,0 +1,28 @@
+// normal function
+
+function funcName(parameter){
+    return
+}
+
+// function expression
+
+const funName = function(pre1,pre2){
+    return
+}
+
+// arrow function 
+
+// const add = (a, b) => a + b
+const add = (a, b) => a * b
+
+// console.log(add(10,8));
+
+
+// multi line arrow function
+
+const ageGap = (age1, age2) => {
+    const result = age1 - age2
+    return result
+} 
+
+// console.log(ageGap(35 , 10));
---
+++ UI-Component/js/template-string.js
@@ -0,0 +1,7 @@
+function sum(num1,num2){
+    const result = num1 + num2;
+    console.log(`Num1 is ${num1} + Num2 is ${num2} = ${result}`);
+    
+}
+
+sum(30,10)
