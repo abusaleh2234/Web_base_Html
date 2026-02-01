a88a1a1a350514bb3bb63610601ebb6727f5d51f
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-01 22:28:19 +0600

    spread operetor

---
+++ UI-Component/js/spread.js
@@ -0,0 +1,12 @@
+const numbers = [21,4,35,4,768,7,453,23,32,455,67,8,7]
+
+const all =[...numbers]
+// console.log(all);
+
+const max = Math.max(...numbers)
+// console.log(max);
+
+const age = [23,21,34,37,17,28]
+
+const allAge = [10,19,...age, 45,88]
+console.log(allAge);
