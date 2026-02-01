9df9fa35354e2985c9d9ccce858f33b99cc5224c
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-01 19:45:10 +0600

    es6 let const

---
+++ UI-Component/js/default-parameter.js
@@ -0,0 +1,17 @@
+
+function sum(num1=0, num2=0){
+    const sum = num1 + num2
+    console.log(sum);
+    
+}
+
+sum(20)
+/*
+// what i use 
+//  1. sum ,abs ====== 0
+    2. * , / ======= 1
+    3. if parameter is [] ==== []
+    3. if parameter is {} ==== {}
+// 
+// 
+*/
--- UI-Component/js/index.js
+++ UI-Component/js/index.js
@@ -1,5 +1,22 @@
 // document.getElementById("subTitle").innerText = " sub title"
 // document.getElementById("subTitle").style.border = "1px solid red"
-document.getElementById("subTitle").style.color = "red"
+// document.getElementById("subTitle").style.color = "red"
 
-//  document.getElementsByClassName("card_content").style.color ="red"j
+//  document.getElementsByClassName("card_content").style.color ="red"
+
+// console.log(text);
+
+var text = "hello"
+
+const name = "rahim";
+// name = "karim"
+// console.log(name);
+const roll = [2,5,8,3,9,34,12]
+// roll = []
+// console.log(roll);
+// console.log(age);
+
+let age = 54;
+age = 55;
+// console.log(age);
+
