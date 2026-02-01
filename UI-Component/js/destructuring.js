fe29ab42aa413a46b0825273a2b7cd67b7d5fd03
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-01 23:11:58 +0600

    some object methode

--- UI-Component/js/destructuring.js
+++ UI-Component/js/destructuring.js
@@ -14,6 +14,32 @@ const number = [10,30,60,20,50,80]
 
 const [first, second,...other ] = number
 // console.log(other);
-console.log(first);
+// console.log(first);
+
+
+// ------Keys, values, entries, delete, seal, freeze----------
+
+
+const student = {name: "Jamal", roll: 20, score: 5, id: 23345}
+
+const Keys = Object.keys(student)
+// console.log(Keys);
+const values = Object.values(student)
+// console.log(values);
+const entries = Object.entries(student)
+// console.log(entries);
+
+
+// Object.freeze(student)
+Object.seal(student)
+delete student.id
+// delete student.roll
+
+student.score = 5+4
+student.group = "science"
+console.log(student);
+
+
+
 
 
