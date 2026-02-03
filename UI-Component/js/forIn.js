d551a568dd3da38706d331678055206d60eddb2b
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-03 19:29:59 +0600

    es6 basic problem

--- UI-Component/js/forIn.js
+++ UI-Component/js/forIn.js
@@ -8,6 +8,25 @@ for (const key in student) {
 const Keys = Object.keys(student)
 
 for (const key of Keys) {
-    console.log(student[key])
+    // console.log(student[key])
 };
-    
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evens = numbers.filter(n => n % 2 === 0);
+// console.log(evens);
+
+const user = {
+  name: "Rahim",
+  age: 22,
+  city: "Dhaka"
+};
+
+const { name, age } = user;
+// console.log(name, age);
+
+const arr = [5, 20, 15];
+
+const max = Math.max(...arr);
+// console.log(max);
