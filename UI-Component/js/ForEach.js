f2aee29ee258e5e14a63ba263acf0ab990fcf94e
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-04 19:42:21 +0600

    file change

--- UI-Component/js/ForEach.js
+++
@@ -1,4 +0,0 @@
-const  number = [21,32,41,,43,53,56,22]
-
-const result = number.forEach(x => console.log(x *2))
-// console.log(result);
--- UI-Component/js/find.js
+++
@@ -1,5 +0,0 @@
-const  number = [21,32,41,,43,53,56,22]
-
-
-const result = number.find(num => num >= 60)
-console.log(result);
