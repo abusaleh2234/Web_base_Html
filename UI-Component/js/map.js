e41fa9f501a15bf6d36407c8f9387c1424a9a528
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-04 17:37:55 +0600

    practice map

---
+++ UI-Component/js/map.js
@@ -0,0 +1,14 @@
+const number = [4,2,5,7,8,5,3,3,6,7]
+
+const eachNum = number.map(x => x*2)
+const SquNum = number.map(x => x*x)
+// console.log(SquNum);
+
+// console.log(eachNum);
+
+
+const friends = ["Naim", "Rakib","Siam","Sajib"]
+
+// const friend = friends.map(friend => console.log(friend))
+const firstLetter = friends.map(friend => friend[0])
+console.log(firstLetter);
