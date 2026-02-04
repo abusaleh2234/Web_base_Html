c270fcbebbe7efc332e23bc4cc46379a7368d475
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-04 18:18:48 +0600

    foreach and filter operation

---
+++ UI-Component/js/ForEach.js
@@ -0,0 +1,4 @@
+const  number = [21,32,41,,43,53,56,22]
+
+const result = number.forEach(x => console.log(x *2))
+// console.log(result);
---
+++ UI-Component/js/filter.js
@@ -0,0 +1,8 @@
+const  number = [21,32,41,,43,53,56,22]
+ const result = number.filter( num => num > 30)
+//  console.log(result);
+
+const even = number.filter( x => x % 2 !== 0)
+console.log(even);
+
+ 
